3f09e619b69a6359786bf10b5e2626d09a37ca57
Nash-Finkbeiner-Student <nafi36a@student.oaisd.org>
2024-12-03 13:49:29 +0000

    Commitning To Main

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+    Console.WriteLine("Hello! \n Welcome to the game! \nTo Start choose one of the three choices to throw out.");
+    gameLoop();
+
+    if(!(looping))
+        {
+            Console.WriteLine("Game Over! \n Number of Wins: " + winCount + " \n Number Of Losses: " + lossCount);
+        }
+    }
+    else
+    {
+        gameLoop();
+    }
+}
