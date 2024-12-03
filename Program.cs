d02ace414f4acd7b6bbfe9d40650292b9e6cb761
carmelo kyles <cakye3c@student.oaisd.org>
2024-12-03 08:52:35 -0500

    adding in nashs code

--- Program.cs
+++ Program.cs
@@ -6,7 +6,18 @@ public static class Program
 {
     public static void Main(string[] args)
     {
+        Console.WriteLine("Hello! \n Welcome to the game! \nTo Start choose one of the three choices to throw out.");
         gameLoop();
+
+        if (!(looping))
+        {
+            Console.WriteLine("Game Over! \n Number of Wins: " + winCount + " \n Number Of Losses: " + lossCount);
+        }
+
+        else
+        {
+            gameLoop();
+        }
     }
     public static Random random = new Random();
     public static int winCount = 0;
