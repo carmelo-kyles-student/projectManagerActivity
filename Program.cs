14b996803174cc28cfbb8d0ab20331a0c136d9e1
Nash-Finkbeiner-Student <nafi36a@student.oaisd.org>
2024-12-04 13:30:21 +0000

    Move All To A Class

--- Program.cs
+++ Program.cs
@@ -8,128 +8,22 @@ public static class Program
     {
         bool looping = true;
         Console.WriteLine("Hello! \nWelcome to the game! \nTo Start choose one of the three choices to throw out.");
+        var play = new GameLoop();
         while(looping)
         {
-            string yn = gameLoop();
+            string yn = GameLoop.gameLoop();
             if(yn.ToUpper() == "Y")
             {
                 looping = true;
             }
             else
             {
+                int wins = play.getwinCount();
+                int losses = play.getlossCount();
+                int ties = play.gettieCount();
                 looping = false;
-                Console.WriteLine("Game Over! \nNumber of Wins: " + winCount + " \nNumber Of Losses: " + lossCount+ " \nNumber Of Ties: " + tieCount);
+                Console.WriteLine("Game Over! \nNumber of Wins: " + wins + " \nNumber Of Losses: " + losses + " \nNumber Of Ties: " + ties);
             }
         }
-    }
-    public static Random random = new Random();
-    public static int winCount = 0;
-    public static int lossCount = 0;
-    public static int tieCount = 0;
-    public static int GenerateRandomNumber() //generate a random number between 0 and 2 
-    {
-
-
-        int AIchoice = random.Next(0, 3); // 0, 1, or 2
-        switch (AIchoice)
-        {
-            case 0:
-                Console.WriteLine("AI chose Rock");
-                break;
-            case 1:
-                Console.WriteLine("AI chose Paper");
-                break;
-            case 2:
-                Console.WriteLine("AI chose Scissors");
-                break;
-
-        }
-        return AIchoice;
-        //0 rock
-        //1 paper 
-        //2 sciscors
-    }
-
-    public static string gameLoop()
-    {
-        int choice = 0;
-        Console.WriteLine("Rock, Paper, or Scissors");
-        string userIn = Console.ReadLine();
-        userIn.ToLower();
-        switch (userIn)
-        {
-            case "rock":
-                choice = 0;
-                break;
-            case "paper":
-                choice = 1;
-                break;
-            case "scissors":
-                choice = 2;
-                break;
-            default:
-                Console.WriteLine("bad statement");
-                break;
-        }
-        int genNum = GenerateRandomNumber();
-
-        switch (choice)
-        {
-            case 0:
-                if (genNum == 0)
-                {
-                    Console.WriteLine("tie");
-                    tieCount++;
-                }
-                else if (genNum == 1)
-                {
-                    Console.WriteLine("lose");
-                    lossCount++;
-
-                }
-                else if (genNum == 2)
-                {
-                    Console.WriteLine("win");
-                    winCount++;
-                }
-                break;
-            case 1:
-                if (genNum == 0)
-                {
-                    Console.WriteLine("win");
-                    winCount++;
-                }
-                if (genNum == 1)
-                {
-                    Console.WriteLine("tie");
-                    tieCount++;
-                }
-                if (genNum == 2)
-                {
-                    Console.WriteLine("loss");
-                    lossCount++;
-                }
-                break;
-            case 2:
-                if (genNum == 0)
-                {
-                    Console.WriteLine("loss");
-                    lossCount++;
-                }
-                else if (genNum == 1)
-                {
-                    Console.WriteLine("win");
-                    winCount++;
-                }
-                else if (genNum == 2)
-                {
-                    Console.WriteLine("tie");
-                    tieCount++;
-                }
-                break;
-        }
-
-        Console.WriteLine("Y/N to going again?");
-        return Console.ReadLine();
     }
 }
